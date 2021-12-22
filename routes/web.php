ae18f211f9d43008d92fc213ffab62b7b6b42397
eddymandran <eddy.mandran@gmail.com>
2021-12-22 13:57:29 +0100

    Section 3 / Fondamentaux / Middleware: mise en place d'un middleware pour tester si l'user est connecté ou non

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('articles', ArticleController::class);
+Route::get('/test', function () {
+    return view('test');
+})->middleware('custom.auth');
