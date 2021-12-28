ff5723abbda74e04d62a12dd81d6eecd3279f6e2
eddymandran <eddy.mandran@gmail.com>
2021-12-28 15:19:31 +0100

    Utilisation de font Awesome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="h-100 p-5 bg-light border rounded-3">
       <h1 class="display-5 fw-bold">Custom jumbotron</h1>
       <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-      <button class="btn btn-primary btn-lg" type="button">Example button</button>
+      <button class="btn btn-primary btn-lg" type="button">Learn more <i class="fas fa-arrow-right"></i> </button>
     </div>
   </div>
 @endsection
